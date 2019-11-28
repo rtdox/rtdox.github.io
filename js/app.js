8ccb943c103809c80a1546fe4fe273f1bfecf8de
rtdox <rtdox@users.noreply.github.com>
2019-11-28 22:24:35 +0200

    Upgrade All

---
+++ js/app.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        //
+    },
+
+    methods: {
+        //
+    }
+});
+
+$(document).ready(function () {
+    $('.hidden_phone').click(function(e) {
+        e.preventDefault();
+        const c = '23';
+        const b = '900';
+        const d = '55';
+        const a = '+38 (093)';
+        let span = a+'ara'+b+'gtg'+c+'gtg'+d;
+        let str = 'telyjy'+span;
+        span = span.replace('ara', ' ').replace('gtg', '-').replace('gtg', '-');
+        str = str.replace('yjy', ':').replace('ara', '').replace('gtg', '').replace('gtg', '').replace(' ', '').replace('(', '').replace(')', '');
+        $(this).attr('href', str);
+        $('#span_for_phone').html(span);
+        $('.hidden_phone').off('click');
+    });
+});
